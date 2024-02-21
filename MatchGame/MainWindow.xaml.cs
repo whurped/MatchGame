d11e31e915836dbc11bf308e29134ea7cff63faa
email <email@KIDUS>
2024-02-21 10:13:03 -0500

    Add project files.

---
+++ MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetupGame();
+        }
+
+        private void SetupGame()
+        {
+            List<string> animalEmoji = new List<string>()
+
+       
+            {
+                "🐙", "🐙",
+                "🐡", "🐡",
+                "🐘", "🐘",
+                "🐳", "🐳",
+                "🐪", "🐪",
+                "🦕", "🦕",
+                "🦘", "🦘",
+                "🦔", "🦔",
+            };
+
+            Random random = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>()) 
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+        }
+    }
+}
